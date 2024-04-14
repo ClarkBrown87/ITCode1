c1895ab14b0939a3b4c43995c47fe0ab78e8649e
Iskander <58907830+ClarkBrown87@users.noreply.github.com>
2024-04-15 00:35:50 +0500

    Complete the task 10

---
+++ step10.py
@@ -0,0 +1,13 @@
+def unique_numbers(our_str):
+    for i in our_str:
+        if i.isdecimal() and our_str.count(i) == 1:
+            continue
+        else:
+            return False
+    return True
+
+user_str = input(str("Введите строку: "))
+if unique_numbers(user_str):
+    print("Все числа в строке уникальны")
+else:
+    print("В строке есть не уникальные числа")
