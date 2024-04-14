58f0496596fd229b2fc138ee2897153d9f7bf370
Iskander <58907830+ClarkBrown87@users.noreply.github.com>
2024-04-15 00:11:16 +0500

    Complete the task 6

---
+++ step6.py
@@ -0,0 +1,9 @@
+def short_fio(user_str):
+    words = user_str.split()
+    if len(words) == 3:
+        new_fio = words[0] + ' ' + words[1][0] + '. ' + words[2][0] + '.'
+        return new_fio
+
+
+user_str = input(str("Введите ФИО полностью: "))
+print(short_fio(user_str))
