65a06e165d927a2a567bb5604097eee595065408
Iskander <58907830+ClarkBrown87@users.noreply.github.com>
2024-04-15 00:02:30 +0500

    Complete the task 4

---
+++ step4.py
@@ -0,0 +1,20 @@
+def find_word(our_str):
+    word = False
+    count_word = 0
+    for char in our_str:
+        if char == ' ':
+            if word:
+                count_word += 1
+                word = False
+                continue
+            continue
+        else:
+            word = True
+
+    if word: count_word += 1
+
+    return count_word
+
+
+user_str = input(str("Введите строку: "))
+print(find_word(user_str))
